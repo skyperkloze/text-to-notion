b0796c1a6d1a3101cbd4018d74e8065389a8ecbf
skyperkloze <57665838+skyperkloze@users.noreply.github.com>
2022-02-08 16:30:49 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from bot import Bot
 def main():
-    updater.dispatcher.logger.addFilter((lambda s: not s.msg.endswith('A TelegramError was raised while processing the Update')))
+   
     bot = Bot()
     bot.run() 
     
